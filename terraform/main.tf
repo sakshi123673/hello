a69ffafd3f8b3a77b8251d710802832c17d7d40b
sakshi123673 <72104818+sakshi123673@users.noreply.github.com>
2023-10-19 23:48:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ access_key = "AKIAXFOFGNIUJAO5KKVE"
 secret_key = "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"
 }
 
-resource "aws_instance" "key" {
+resource "aws_instance""key" {
 ami = "ami-023e1b810cda5ce69"
 instance_type = "t2.micro"
 key_name = "oct19"
