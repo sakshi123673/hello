6151758708db08a696eceef029f76d4934dc8a22
sakshi123673 <72104818+sakshi123673@users.noreply.github.com>
2023-10-19 23:55:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,11 @@ secret_key = "78H5xiN1GR/61dTgLDn+r/G4O73i4l0w63Z2tJZx"
 
 resource "aws_instance" "key" {
 ami = "ami-023e1b810cda5ce69"
-instance_type = "t2.micro"
+instance_type = "t2.micr0"
 key_name = "oct19"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
 Name = "project_instance"
 }
 }
+
