fd92035487f5bd484cb8b28896e058b5344ab62e
sakshi123673 <72104818+sakshi123673@users.noreply.github.com>
2023-10-19 23:57:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+iprovider "aws" {
 region = "ap-southeast-2"
 access_key = "AKIAXFOFGNIUJAO5KKVE"
 secret_key = "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"
@@ -6,7 +6,7 @@ secret_key = "78H5xiN1GR/61dTgLDn+r/G4O73i4l0w63Z2tJZx"
 
 resource "aws_instance" "key" {
 ami = "ami-023e1b810cda5ce69"
-instance_type = "t2.micr0"
+instance_type = "t2.micro"
 key_name = "oct19"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
